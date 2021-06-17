5e2a137734bbbc1566d3508647752684cf6c9455
welch <welchwsy@gmail.com>
2021-06-17 09:49:53 +0800

    add device_manage_query的接口

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const API = require('./lib/api_common');
 
 API.mixin(require('./lib/api_account'));
 API.mixin(require('./lib/api_device_operate'));
+API.mixin(require('./lib/api_device_manage_query'));
 
 module.exports = API;
